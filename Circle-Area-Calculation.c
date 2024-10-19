6cc4b5a02ee4150665f4cf42fcdcc8f1cb4a6133
minakdr <78243545+minakdr@users.noreply.github.com>
2024-10-19 18:07:12 +0100

    Update and rename main.c to Circle-Area-Calculation.c

--- Circle-Area-Calculation.c
+++ Circle-Area-Calculation.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 
+// The program always starts with int main ()
 // On commance toujours un programme avec int main ()
-int main () {
-   //on commence par declarer les constantes
-   const float pi = 3.14 ;
-   //ensuite les variables
-   float rayon, surface;
 
-  // Traitement
+int main () {
+   // First, we declare constants
+   // On commence par declarer les constantes
+   const float pi = 3.14 ;
+   // Then we declare the variables
+   // Ensuite les variables
+   float rayon, surface;
+   
+   // Processing
+   // Traitement
+   
    printf("Entrez le rayon du cercle : ");
    scanf("%f", &rayon);
-   //calcul du resultat
+   
+   // Calculation of the result
+   // Calcul du resultat
+   
    surface = rayon*rayon*pi;
+   
    //affichage du resultat
+   // Display the result
+   
    printf("voici la surface %f " , surface);
 
-
-
    // On termine toujours le programme par return 0
+   // We always end the program with return 0
 
    return 0;
 
