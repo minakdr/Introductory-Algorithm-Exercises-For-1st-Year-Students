a7689ac8b425d4ce94f38be65314343d2a5a5e6f
minakdr <78243545+minakdr@users.noreply.github.com>
2024-10-26 19:38:47 +0100

    Update and rename main.c to temperature_conversion.c

--- temperature_conversion.c
+++ temperature_conversion.c
@@ -3,24 +3,24 @@
 int main()
 {
     // Declare variables for Celsius and Fahrenheit temperatures
-    // Déclarer des variables pour les températures en Celsius et en Fahrenheit
+    // DÃ©clarer des variables pour les tempÃ©ratures en Celsius et en Fahrenheit
     float C, F;
 
     // Prompt the user to enter the temperature in Celsius
-    // Demander à l'utilisateur de saisir la température en Celsius
-    printf("Donnez la température en Celsius: ");
+    // Demander Ã  l'utilisateur de saisir la tempÃ©rature en Celsius
+    printf("Donnez la tempÃ©rature en Celsius: ");
 
     // Read the Celsius temperature entered by the user
-    // Lire la température en Celsius saisie par l'utilisateur
+    // Lire la tempÃ©rature en Celsius saisie par l'utilisateur
     scanf("%f", &C);
 
     // Convert Celsius temperature to Fahrenheit
-    // Convertir la température de Celsius en Fahrenheit
+    // Convertir la tempÃ©rature de Celsius en Fahrenheit
     F = C * 9 / 5 + 32;
 
     // Display the temperature in Fahrenheit
-    // Afficher la température en Fahrenheit
-    printf("Voici la température en Fahrenheit: %f\n", F);
+    // Afficher la tempÃ©rature en Fahrenheit
+    printf("Voici la tempÃ©rature en Fahrenheit: %f\n", F);
 
     // End of the program
     // Fin du programme
